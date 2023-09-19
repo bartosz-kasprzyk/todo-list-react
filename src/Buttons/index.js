d52e4e6e652f0cad46c2f6e2d1dbc771d7c8b1bb
Bartosz Kasprzyk <bartosz.kasprzyk58@gmail.com>
2023-09-19 21:42:56 +0200

    deleted unnecessary content

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -20,24 +20,3 @@ const Buttons = ({ tasks, hideDoneTasks }) => (
 );
 
 export default Buttons;
-
-
-//const Buttons = ({ tasks, hideDoneTasks }) => {
-//    if (tasks.length === 0) {
-//        return null;
-//    }
-//    return (
-//        <span className="buttons">
-//            <button className="buttons__button">
-//                {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
-//            </button>
-//            <button
-//            className="buttons__button"
-//            disabled={tasks.every(({ done }) => done)}
-//            >
-//                Ukończ wszystkie
-//            </button>
-//        </span>
-//    )
-//};
-//export default Buttons;
