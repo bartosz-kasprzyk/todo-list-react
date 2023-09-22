726d36c435e252b85cd5b72d071b6765b87c3c4d
Bartosz Kasprzyk <bartosz.kasprzyk58@gmail.com>
2023-09-22 19:54:27 +0200

    toggleTaskDone interaction

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,11 +1,13 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone, removeTask }) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li
                 className={`tasks__item ${task.done && hideDone ? "tasks__hidden" : ""}`} key={task.id}>
-                <button className="tasks__button tasks__button--done">
+                <button
+                className="tasks__button tasks__button--done"
+                onClick={() => toggleTaskDone(task.id)}>
                     <i className={`${task.done ? "gg-check" : "tasks__button--disabled"}`}></i>
                 </button>
                 <span className={`tasks__content ${task.done ? "tasks__item--done" : ""}`}>
