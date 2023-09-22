80378c0a4ef38cdd4e991ec4c79aab8094090030
Bartosz Kasprzyk <bartosz.kasprzyk58@gmail.com>
2023-09-22 19:40:19 +0200

    toggleHideDone interaction

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -6,30 +7,43 @@ import Header from "./Header";
 import Container from "./Container";
 
 const tasks = [
-  {id: 1, content: "test 1", done: false},
-  {id: 2, content: "test 2", done: true},
+  { id: 1, content: "test 1", done: false },
+  { id: 2, content: "test 2", done: true },
 ];
 
-const hideDone = false;
-
 function App() {
-  return (
-    <Container>
-      <Header title="Lista zadań" />
-      <main>
-        <Section
-          title="Dodaj nowe zadanie"
-          body={<Form />}
-        />
+  const [hideDone, setHideDone] = useState(false);
 
-        <Section
-          title="Lista zadań"
-          body={<Tasks tasks={tasks} hideDone={hideDone} />}
-          extraContent={<Buttons tasks={tasks} hideDone={hideDone}/>}
-        />
-      </main>
-    </Container>
-  );
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+};
+
+return (
+  <Container>
+    <Header title="Lista zadań" />
+    <main>
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+      />
+
+      <Section
+        title="Lista zadań"
+        body={
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone} />
+        }
+        extraContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+          />}
+      />
+    </main>
+  </Container>
+);
 }
 
 export default App;
