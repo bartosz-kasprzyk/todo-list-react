e9c192176bffcd596d9aaf5a7a03d283ab02bc20
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2023-10-15 19:06:16 +0200

    added custom hook "useTasks"

---
+++ src/useTasks.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+export const useTasks = () => {
+    const localStorageTasksKey = "tasks";
+    const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem(localStorageTasksKey)) || []);
+
+    useEffect(() => {
+        localStorage.setItem(localStorageTasksKey, JSON.stringify(tasks));
+    }, [tasks])
+
+    const removeTask = (id) => {
+        setTasks(tasks => tasks.filter(task => task.id !== id));
+    };
+
+    const toggleTaskDone = (id) => {
+        setTasks(tasks => tasks.map(task => {
+            if (task.id === id) {
+                return { ...task, done: !task.done };
+            }
+            return task;
+        }));
+    };
+
+    const setAllDone = () => {
+        setTasks(tasks => tasks.map(task => ({
+            ...task,
+            done: true,
+        })));
+    };
+
+    const addNewTask = (content) => {
+        setTasks(tasks => [
+            ...tasks,
+            {
+                content,
+                done: false,
+                id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1
+            }
+        ])
+    };
+
+    return { tasks, removeTask, toggleTaskDone, setAllDone, addNewTask };
+};
