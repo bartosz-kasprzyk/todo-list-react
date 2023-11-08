6e2bd69d16c8ef2a9459ac11dc49c49c0451467f
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2023-11-08 18:33:29 +0100

    application split into features

--- src/index.js
+++ src/index.js
@@ -5,6 +5,42 @@ import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
+import { configureStore } from "@reduxjs/toolkit";
+
+const initialState = {
+  tasks: [],
+};
+
+const tasksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "addTask":
+      return{
+        ...state,
+        tasks: [
+          ...state.tasks,
+          {
+            content: action.payload,
+          },
+        ],
+      };
+
+      default:
+        return state;
+  }
+};
+
+const addTask = content => ({
+  type: "addTask",
+  payload: content,
+});
+
+const selectTasks = ({tasks}) => tasks;
+
+const store = configureStore({reducer: tasksReducer});
+console.log(selectTasks(store.getState()));
+
+store.dispatch(addTask("zadanie 1"));
+console.log(selectTasks(store.getState()));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
