fde792c719ff541d81e42c92fd105d150188f79a
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2023-11-29 19:22:34 +0100

    added saving and downloading tasks from localStorage

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,23 @@
+import { takeEvery, takeLatest, call, put, select } from "redux-saga/effects";
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice"
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+
+function* fetchExampleTasksHandler() {
+    try {
+        const exampleTasks = yield call(getExampleTasks);
+        yield put(setTasks(exampleTasks));
+    } catch (error) {
+        yield call(alert, error);
+    }
+}
+
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
