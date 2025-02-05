00890acbfb7dbf9e86657c45e1845dbfecfd0485
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2025-02-05 17:31:29 +0100

    test rendering and content of AuthorPage

---
+++ tests/components/AuthorPage.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { AuthorPage } from "../../src/features/AuthorPage";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../../src/theme";
+
+describe("AuthorPage", () => {
+  it("should render Header and Section with correct content", () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <AuthorPage />
+      </ThemeProvider>
+    );
+
+    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+      "O autorze"
+    );
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Bartosz Kasprzyk"
+    );
+    expect(screen.getByText(/magister matematyki/i)).toBeInTheDocument();
+    expect(screen.getByText(/front-end developer/i)).toBeInTheDocument();
+  });
+});
